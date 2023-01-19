2b70a10c9b00227cd8f5e3e70b3b230fb13032a5
GodlyCarrot2 <rquan80@ladueschools.net>
2023-01-19 15:33:01 +0000

    aaron is sitting next to me

---
+++ Cereal.java
@@ -0,0 +1,39 @@
+public class Cereal {
+    String name;
+    String type;
+    int calories;
+    int protein;
+    int fat;
+    int sodium;
+    int fiber;
+    int carbs;
+    int sugar;
+    int potassium;
+    int vitamins;
+    int shelf;
+    Double weight;
+    Double cups;
+    Double rating;
+
+    public Cereal() {
+        name = "Name";
+        type = " ";
+        calories = 0;
+        protein = 0;
+        fat = 0;
+        sodium = 0;
+        fiber = 0;
+        carbs = 0;
+        sugar = 0;
+        potassium = 0;
+        vitamins = 0;
+        shelf = 0;
+        weight = 0.0;
+        cups = 0.0;
+        rating = 0.0;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
